b644f047f9fa745683d53aa54b81c237de401331
Henry Jackson <henry.jackson.95@gmail.com>
2018-09-14 10:34:29 -0400

    creates files from templates

--- builder.py
+++ builder.py
@@ -10,6 +10,14 @@ DIRS = [
     '{project_slug}/templates',
 ]
 
+FILES = {
+    '{project_slug}/requirements.txt': 'requirements.txt.template',
+    '{project_slug}/app.py': 'app.py.template',
+    '{project_slug}/static/css/{project_slug}.css': 'project.css.template',
+    '{project_slug}/static/js/{project_slug}.js': 'project.js.template',
+    '{project_slug}/templates/index.html': 'index.html.template'
+}
+
 def slugify(string):
     string = unicodedata.normalize('NFKC', string)
     string = re.sub(r'[^\w\s]', '', string).strip().lower()
@@ -54,6 +62,24 @@ def create_dirs(root, slug):
                 os.mkdir(os.path.join(root, dir.format(project_slug=slug)))
             except FileExistsError:
                 pass
+
+
+def create_files(root, slug, name):
+    for file_name, template_name in FILES.items():
+        try:
+            template_file = open(os.path.join('templates', template_name))
+            file_content = template_file.read()
+            file_content = flask_template_prepare(file_content)
+            file_content = file_content.format(project_name=name, project_slug=slug)
+            file_content = flask_template_repair(file_content)
+
+            target_file = open(os.path.join(root, file_name.format(project_slug=slug)), 'w')
+            target_file.write(file_content)
+        except OSError:
+            print("Couldn't create {}".format(file_name.format(project_slug=slug)))
+        finally:
+            template_file.close()
+            target_file.close()
 
 
 def main():
